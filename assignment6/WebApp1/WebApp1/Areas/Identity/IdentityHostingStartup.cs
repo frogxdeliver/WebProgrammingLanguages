e64240e3811e5722e55f77170df8497cc7b1597c
frogxdeliver <33261077+frogxdeliver@users.noreply.github.com>
2019-03-14 16:41:55 -0400

    hmwk 6

---
+++ assignment6/WebApp1/WebApp1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApp1.Data;
+
+[assembly: HostingStartup(typeof(WebApp1.Areas.Identity.IdentityHostingStartup))]
+namespace WebApp1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                /*services.AddDefaultIdentity<IdentityUser>(config =>
+                {
+                    config.SignIn.RequireConfirmedEmail = true;
+                });*/
+            });
+        }
+
+        /*public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<WebPWrecoverContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("WebPWrecoverContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(config =>
+                {
+                    config.SignIn.RequireConfirmedEmail = true;
+                })
+                    .AddEntityFrameworkStores<WebPWrecoverContext>();
+            });
+        }*/
+    }
+}
